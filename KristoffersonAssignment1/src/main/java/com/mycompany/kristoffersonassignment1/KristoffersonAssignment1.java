aa27ee30ba2d8ec3d8990ecc5fe5f2b8823f36c0
Rachel <Rachel@localhost>
2025-07-01 22:00:33 -0500

    seeing if git works

---
+++ KristoffersonAssignment1/src/main/java/com/mycompany/kristoffersonassignment1/KristoffersonAssignment1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kristoffersonassignment1;
+
+/**
+ *
+ * @author Rachel
+ */
+public class KristoffersonAssignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Does this work");
+    }
+}
